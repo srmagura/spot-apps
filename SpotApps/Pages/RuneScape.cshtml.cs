bdb3804a95939c538458d8af6db5b89151240172
Sam Magura <srmagura@gmail.com>
2022-05-28 11:43:50 -0400

    Call Jagex API

--- SpotApps/Pages/RuneScape.cshtml.cs
+++ SpotApps/Pages/RuneScape.cshtml.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.RegularExpressions;
 
 namespace SpotApps.Pages;
 
 public class RuneScapeModel : PageModel
 {
+    private static readonly HttpClient _httpClient = new();
+
     public string PlayerName { get; } = "wh1teberry";
     public int Rank { get; private set; }
     public int Xp { get; private set; }
 
     public async Task OnGetAsync()
     {
-        Rank = 117029;
-        Xp = 580123123;
+        var url = $"https://secure.runescape.com/m=hiscore/index_lite.ws?player={PlayerName}";
+        var text = await _httpClient.GetStringAsync(url);
+
+        var match = Regex.Match(text, @"(\d+),\d+,(\d+)");
+
+        if (match == null)
+        {
+            throw new Exception("Failed to parse result.");
+        }
+
+        Rank = int.Parse(match.Groups[1].Value);
+        Xp = int.Parse(match.Groups[2].Value);
     }
 }
