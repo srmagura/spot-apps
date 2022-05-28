e33fffb30b3722d2e87ed67640f5b81f2b55faa9
Sam Magura <srmagura@gmail.com>
2022-05-28 11:35:28 -0400

    Runescape styling

--- SpotApps/Pages/RuneScape.cshtml.cs
+++ SpotApps/Pages/RuneScape.cshtml.cs
@@ -5,4 +5,13 @@ namespace SpotApps.Pages;
 
 public class RuneScapeModel : PageModel
 {
+    public string PlayerName { get; } = "wh1teberry";
+    public int Rank { get; private set; }
+    public int Xp { get; private set; }
+
+    public async Task OnGetAsync()
+    {
+        Rank = 117029;
+        Xp = 580123123;
+    }
 }
